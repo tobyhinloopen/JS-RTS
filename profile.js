acf12ada3ea164680125f696f846189ed001919b
Toby Hinloopen <toby@zisoo.nl>
2015-07-18 23:23:24 +0200

    Its SLOW

--- profile.js
+++ profile.js
@@ -2,6 +2,6 @@ var WorldMapFactory = require('world_map_factory');
 var PathFinder = require('path_finder');
 var pathFinder = new PathFinder(WorldMapFactory.buildJsRtsSampleMap());
 
-var COUNT = 400;
+var COUNT = 10;
 for(var i=0; i<COUNT; i++)
   pathFinder.find({ x: 9, y: 13 }, { x: 13, y: 0 });
