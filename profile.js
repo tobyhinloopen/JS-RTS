8ea2d7ce7190fc4189888f132f365b08771ab9fc
Toby Hinloopen <toby@zisoo.nl>
2015-07-18 23:22:42 +0200

    Profiling special use-case

--- profile.js
+++ profile.js
@@ -3,8 +3,5 @@ var PathFinder = require('path_finder');
 var pathFinder = new PathFinder(WorldMapFactory.buildJsRtsSampleMap());
 
 var COUNT = 400;
-for(var i=0, points = []; i<COUNT+1; i++)
-  points.push({ x: (Math.random()*16)|0, y: (Math.random()*16)|0 });
-
-while(points.length > 2)
-  pathFinder.find(points.shift(), points[0]);
+for(var i=0; i<COUNT; i++)
+  pathFinder.find({ x: 9, y: 13 }, { x: 13, y: 0 });
