fbb315d848c5dd85ae4ba02802b6c581058dde4e
Toby Hinloopen <toby@zisoo.nl>
2015-07-23 12:59:48 +0200

    Profile'd

--- profile.js
+++ profile.js
@@ -1,7 +1,1 @@
-var WorldMapFactory = require('world_map_factory');
-var PathFinder = require('path_finder');
-var pathFinder = new PathFinder(WorldMapFactory.buildJsRtsSampleMap());
-
-var COUNT = 10;
-for(var i=0; i<COUNT; i++)
-  pathFinder.find({ x: 9, y: 13 }, { x: 13, y: 0 });
+require('./bench');
