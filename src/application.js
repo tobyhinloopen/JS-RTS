eb5ae21a2a8b5032f99bfe41031192adfdf81011
Toby Hinloopen <toby@zisoo.nl>
2015-10-28 21:42:38 +0100

    other map

--- src/application.js
+++ src/application.js
@@ -1,7 +1,7 @@
 var GameApplication = require('game_application');
 var ImageWorldMapReader = require('image_world_map_reader');
 
-ImageWorldMapReader.load('maps/8room_005_cropped.png', function(worldMap) {
+ImageWorldMapReader.load('maps/sample-map-dotted.png', function(worldMap) {
   var app = new GameApplication(worldMap);
   document.body.appendChild(app.view);
   app.load(app.start.bind(app));
